ff1e48f6c868eb6103baa14a6c769e23f3154358
JohnZBurnett <tuninator@gmail.com>
2018-05-15 15:01:07 -0400

    added the search function

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -2,12 +2,13 @@ require 'rest-client'
 require 'json'
 require 'pry'
 
-def get_character_movies_from_api(character)
+def get_character_movies_from_api(search_value)
 
-  character_hash = get_info_from_api
+  character_hash = get_info_from_api(search_value)
+  binding.pry
 
   films_array = character_hash["results"].find do |character_data|
-    character_data["name"].downcase == character
+    character_data["name"].downcase == search_value["name"]
   end
 
   if films_array == nil
@@ -24,9 +25,17 @@ def get_film_array_from_api(films)
   end
 end
 
-def get_info_from_api
-  all_characters = RestClient.get('http://www.swapi.co/api/people/')
+def get_info_from_api(search_value)
+  all_characters = RestClient.get("http://www.swapi.co/api/#{search_value["search_type"]}/")
   JSON.parse(all_characters)
+
+  page = 1
+  until JSON.parse(all_characters)["next"] == null
+      all_characters = RestClient.get("http://www.swapi.co/api/#{search_value["search_type"]}/?page=#{page}")
+      JSON.parse(all_characters)
+      page += 1
+    end
+
 end
 
 def parse_character_movies(films_hash)
@@ -35,8 +44,8 @@ def parse_character_movies(films_hash)
   end
 end
 
-def show_character_movies(character)
-  films_hash = get_character_movies_from_api(character)
+def show_character_movies(search_value)
+  films_hash = get_character_movies_from_api(search_value)
   if films_hash == nil
     return puts "The character could not be found!"
   end
--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,10 +1,18 @@
+require 'pry'
+
 def welcome
   puts "Welcome to this Star Wars app!"
 end
 
 def get_character_from_user
-  puts "please enter a character"
-  gets.chomp.downcase
+  puts "please choose people, planets, films, species, vehicles, or starships"
+  search_type = gets.chomp.downcase
+
+  puts "please enter a name"
+  name = gets.chomp.downcase
   #"luke skywalker"
   # use gets to capture the user's input. This method should return that input, downcased.
+ {  "search_type" => search_type, "name" => name  }
+
+
 end
