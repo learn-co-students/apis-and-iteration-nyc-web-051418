ad802441dc03648ad860c9f518904dcd856f1cc5
squamuglia <maxsmouha@gmail.com>
2018-05-15 13:01:29 -0400

    Done.

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -6,7 +6,6 @@ def get_character_movies_from_api(character)
   #make the web request
   all_characters = RestClient.get('http://www.swapi.co/api/people/')
   character_hash = JSON.parse(all_characters)
-  
   # iterate over the character hash to find the collection of `films` for the given
   #   `character`
   # collect those film API urls, make a web request to each URL to get the info
@@ -16,10 +15,29 @@ def get_character_movies_from_api(character)
   # this collection will be the argument given to `parse_character_movies`
   #  and that method will do some nice presentation stuff: puts out a list
   #  of movies by title. play around with puts out other info about a given film.
+
+  results = character_hash["results"]
+  character_info = results.select do |characters|
+    characters["name"] ==  character
+  end
+
+  films = character_info[0]["films"]
+
+  films.map do |film|
+    film_info = RestClient.get("#{film}")
+    film_p = JSON.parse(film_info)
+
+  end
 end
 
 def parse_character_movies(films_hash)
-  # some iteration magic and puts out the movies in a nice list
+  films_hash.each do |film|
+    puts "Title: #{film["title"]}"
+    puts "Director: #{film["director"]}"
+    puts "Release: #{film["release_date"]}"
+    puts "Crawl: #{film["opening_crawl"][0..150]}"
+    puts "\n"
+  end
 end
 
 def show_character_movies(character)
