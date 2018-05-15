c92a66f5c531b3c6060cff9d69e5e6067d115960
github username <github email address>
2018-05-15 16:25:08 -0400

    Done.

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -4,58 +4,42 @@ require 'pry'
 
 
 def find_character(page_num, character)
-  #make the web request on input page
-  all_characters = RestClient.get("http://www.swapi.co/api/people/?#{page_num}")
+  all_characters = RestClient.get("http://www.swapi.co/api/people/?page=1")
   character_hash = JSON.parse(all_characters)
-  character_info = character_hash["results"]
 
-  #check to see if name matches
   char_hash = {}
-  character_info.each do |info|
-    if info["name"].downcase == character
-      char_hash = info
-      return char_hash
+  while character_hash["next"] != nil
+    #make the web request on input page
+    all_characters = RestClient.get("http://www.swapi.co/api/people/?page=#{page_num}")
+    character_hash = JSON.parse(all_characters)
+    character_info = character_hash["results"]
+
+    #check to see if name matches
+    character_info.each do |info|
+      if info["name"].downcase == character
+        char_hash = info
+        return char_hash
+      end
     end
+    page_num += 1
   end
-
-  if !character_hash["next"]
-    puts "Your character doesn't exist"
-    abort()
-  end
-
+  puts "Invalid input."
+  abort()
 end
 
 
 def get_character_movies_from_api(character)
-  all_characters = RestClient.get("http://www.swapi.co/api/people/?page=1")
-  character_hash = JSON.parse(all_characters)
-
-  # iterate over the character hash to find the collection of `films` for the given
-  #   `character`
-  character_info = character_hash["results"]
   page_num = 1
   char_hash = find_character(page_num, character)
 
-  while true
-    if char_hash == {}
-      page_num += 1
-      char_hash = find_character(page_num, character)
-    else
-      break
-    end
+  movie_array = char_hash["films"]
+  movie_info = []
+  movie_array.each do |link|
+  movie_link = RestClient.get(link)
+  movie_info_hash = JSON.parse(movie_link)
+  movie_info << movie_info_hash
   end
-
-  if char_hash != nil
-    movie_array = char_hash["films"]
-    movie_info = []
-    movie_array.each do |link|
-      movie_link = RestClient.get(link)
-      movie_info_hash = JSON.parse(movie_link)
-      movie_info << movie_info_hash
-    end
-    return movie_info
-  end
-  return nil
+  return movie_info
 end
 
 def convert_list_of_API(api_list)
